5126fa15fcb5c3266f04310da462687ccecb3817
deliska cantika <if24.deliskamahendra@mhs.ubpkarawang.ac.id>
2025-11-14 15:48:01 +0700

    pake konstruktor

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,22 +1,26 @@
 <?php
 
+
 class LuasLingkaran{
 
     public const phi = 3.14;
     public int $jari;
 
-    public function tampil($nama ='ban')  {
+    public function __construct($isijari = 1) {
+        $this->jari = $isijari;        
+    }
+
+    public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Lingkaran {$nama} hasilnya adalah: ". $rumus;
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
     }
 
     public static function testing() {
         echo "<br/>";
         echo "ini dari static";
     }
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ahh cape";
+    }
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 7;
-$lingkaran->tampil('roda'); //panggil method
-LuasLingkaran::testing();
